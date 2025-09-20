70656f32dff4fe2ce7b1533144788e7acbeb97e2
Dylan Vidal <dylanvidal1204@gmail.com>
2025-09-19 20:59:42 -0400

    db table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
+import { redirect } from "next/navigation";
 import { auth } from "~/server/auth";
-import { api } from "~/trpc/server";
 import Landing from "./_components/landing";
 
 export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
   const session = await auth();
-
   if (session?.user) {
-    void api.post.getLatest.prefetch();
+    redirect("/life");
   }
 
   return <Landing />;
